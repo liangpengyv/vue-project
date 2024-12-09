a2529d32cebc9e3092d9ba65bac285272efdcca7
liangpengyv <me@laoliang.ink>
2024-12-09 17:14:28 +0800

    feat: 优化 PagContainer 组件

--- src/components/PagContainer.vue
+++ src/components/PagContainer.vue
@@ -1,24 +1,28 @@
 <template>
-  <canvas ref="pag"></canvas>
+  <canvas :id="id" ref="pag"></canvas>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { PAGInit } from 'libpag'
-import { onUnmounted, useTemplateRef, watch } from 'vue'
+import type { PAG } from 'libpag/types/types'
+import type { PAGView } from 'libpag/types/pag-view'
+import { onActivated, onDeactivated, onMounted, onUnmounted, useTemplateRef, watch } from 'vue'
 
-const props = defineProps({
-  url: String,
-  width: Number,
-  height: Number,
-})
+const props = defineProps<{
+  url: string
+  width?: number
+  height?: number
+  id?: string
+}>()
 
 const pagCanvasRef = useTemplateRef('pag')
 
-let PAG
-let pagView
-async function loadPag() {
-  if (!PAG)
-    PAG = await PAGInit({
+let pagInstance: PAG | null
+let pagView: PAGView | null
+
+async function initPAG() {
+  if (!pagInstance) {
+    pagInstance = await PAGInit({
       locateFile(file) {
         if (file === 'libpag.wasm') {
           return '/libpag.wasm'
@@ -26,25 +30,62 @@ async function loadPag() {
         return file
       },
     })
-  if (pagView) pagView.destroy()
+  }
+}
+
+function destroyPrevPagInstance() {
+  if (pagInstance) {
+    pagInstance = null
+  }
+}
+
+async function loadPagView() {
+  if (!pagCanvasRef.value) return
+  await initPAG()
+  destroyPrevPagView()
 
   const blob = await (await fetch(props.url)).blob()
   const file = new window.File([blob], props.url)
-  const pagFile = await PAG.PAGFile.load(file)
+  const pagFile = await pagInstance!.PAGFile.load(file)
+
   pagCanvasRef.value.width = props.width ?? pagFile.width()
   pagCanvasRef.value.height = props.height ?? pagFile.height()
-  pagView = await PAG.PAGView.init(pagFile, pagCanvasRef.value)
+
+  pagView = (await pagInstance!.PAGView.init(pagFile, pagCanvasRef.value)) as PAGView
   pagView.setRepeatCount(0)
   await pagView.play()
 }
 
+function destroyPrevPagView() {
+  if (pagView) {
+    pagView.destroy()
+    pagView = null
+  }
+}
+
 watch(
   () => props.url,
-  () => loadPag(),
-  { immediate: true },
+  () => loadPagView(),
 )
 
+onMounted(() => {
+  loadPagView()
+})
+
 onUnmounted(() => {
-  if (pagView) pagView.destroy()
+  destroyPrevPagView()
+  destroyPrevPagInstance()
+})
+
+onActivated(async () => {
+  if (pagView && !pagView.isPlaying) {
+    await pagView.play()
+  }
+})
+
+onDeactivated(() => {
+  if (pagView) {
+    pagView.pause()
+  }
 })
 </script>
